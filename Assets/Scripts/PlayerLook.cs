60ea509da53c7ec1442934163b437a36097d8838
Simen <simenwiig0@gmail.com>
2021-02-10 18:07:45 +0100

    Player Movement and Camera

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Transform player;
+
+    public float MouseSensitivity = 10;
+
+    private float x = 0;
+    private float y = 0;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        //input
+        x += -Input.GetAxis("Mouse Y") * MouseSensitivity;
+        y += Input.GetAxis("Mouse X") * MouseSensitivity;
+
+
+        //clamping
+        x = Mathf.Clamp(x, -90, 90);
+
+        //rotation
+        transform.localRotation = Quaternion.Euler(x, 0, 0);
+        player.transform.localRotation = Quaternion.Euler(0, y, 0);
+
+        //cursorLocking
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            if (Cursor.lockState == CursorLockMode.None)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float moveSpeed = 6;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        //input
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float y = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        //moving
+        Vector3 movePos = transform.right * x + transform.forward * y;
+        Vector3 newMovePos = new Vector3(movePos.x, rb.velocity.y, movePos.z);
+
+        rb.velocity = newMovePos;
+    }
+}
